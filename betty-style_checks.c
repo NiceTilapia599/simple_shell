4af07ec58e109bdaec24ea15df6633e4791b0fb8
Dennis <deniskiprono53@gmail.com>
2023-05-15 05:10:02 -0700

    Code that passes betty style checks

---
+++ betty-style_checks.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Description - beautiful code that passes betty checks
+ *Return: 0
+ */
+
+int main() 
+{
+	printf("Hello World!\n");
+	printf("We are learning C simple shell.");
+	return 0;
+}
